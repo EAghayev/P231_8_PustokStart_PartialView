f2adbe25fbbcfd1c27b13f2dd9119d9eb7526173
Elchin Aghayev <Elchin@code.local>
2023-06-12 17:39:44 +0400

    project added

---
+++ Pustok/Pustok/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.DAL;
+using Pustok.Models;
+
+namespace Pustok.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly PustokDbContext _context;
+
+        public BookController(PustokDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult GetDetail(int id)
+        {
+            Book book = _context.Books.Include(x=>x.BookImages).FirstOrDefault(x => x.Id == id);
+            //return Json(book);
+            return PartialView("_BookModalPartial", book);
+        }
+    }
+}
